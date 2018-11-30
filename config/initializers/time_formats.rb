67aa9cd8adc63c92da4f3c83ce48573f8ced5f04
Tatsuya Ichikawa <>
2018-11-30 22:46:51 +0900

    [Add] Add time zone setting

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,5 @@
+Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:date] = '%Y年%m月%d日'
+Time::DATE_FORMATS[:time] = '%H:%M:%S'
+Date::DATE_FORMATS[:default] = '%Y/%m/%d'
